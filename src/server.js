3cfb30494cd91a65dcdcd88aa06d58b38aade275
chartaseus <90235896+chartaseus@users.noreply.github.com>
2025-01-22 08:07:14 +0700

    Setup Postgres dan membuat database dengan teknik migrate

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const NotesService = require('./services/inMemory/notesService');
 const notes = require('./api/notes');
@@ -7,8 +8,8 @@ const ClientError = require('./exceptions/ClientError');
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
